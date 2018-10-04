a8f7cb9141c1acaf7e539aa215bbb7005ba87985
Guillaume Coré <gucore@redhat.com>
2018-10-04 16:04:09 +0200

    Service Name change to {GUID}_COMPLETED, fix delete_svc_guid.sh

--- opentlc/delete_svc_guid.sh
+++ opentlc/delete_svc_guid.sh
@@ -35,7 +35,7 @@ guid="$1"
 # you can order a service with regular user,
 # but you cannot retire the service. This is a workaround.
 "${ORIG}/get_svc.sh" \
-    | grep -E "${guid}\$" \
+    | grep -E "${guid}(\$|_)" \
     | (export username=${admin_username}
        export password=${admin_password}
        "${ORIG}/delete_svc.sh")
