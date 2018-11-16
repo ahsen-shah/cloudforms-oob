bfb80318f38acb2f98fee087e6573a7b04e3e2c0
Guillaume Coré <gucore@redhat.com>
2018-11-16 14:24:09 +0100

    Use request_tasks for GUID mapping
    Don't "guess" the GUID, use the request_tasks and 'destination_id' instead.

--- opentlc/order_svc_guid.sh
+++ opentlc/order_svc_guid.sh
@@ -22,36 +22,24 @@ export password
 service_request_id=$("${ORIG}/order_svc.sh" -y "$@" |jq -r '.results[].id')
 
 for i in $(seq 40); do
-    status=$(cfget "${uri}/api/service_requests/${service_request_id}" \
-                 | jq -r '.request_state + "-" + .message')
+    # Use request_tasks to get the service id (destination_id)
+    service_id=$(cfget "${uri}/api/service_requests/${service_request_id}/request_tasks?expand=resources" \
+          | jq -r '.resources[].destination_id')
 
-    #if [[ "$status" =~ 'Service_Template_Provisioning failed' ]]; then
-    #    echo ERROR >&2
-    #    exit 2
-    #fi
-
-    if [ "$status" = "active-In Process" ]; then
+    if [ -n "$service_id" ]; then
         break
     fi
 
     sleep "$i"
 done
 
-[ "$status" = "active-In Process" ]
-
-get_token
-
-# workaround to get GUID: wait for the name to be changed
-# (until guid is set back into the service_request, so it can be query properly
-# using service_request_id)
-
-# get the last service ID
-service_id=$("${ORIG}/get_svc.sh" | grep "${username}" | sort -n | tail -n1 | cut -d' ' -f 1)
+[ -n "${service_id}" ]
 
 for i in $(seq 30); do
-    GUID=$("${ORIG}/get_svc.sh" \
-               | grep "${service_id}" \
+    GUID=$(cfget "${uri}/api/services/${service_id}" \
+               | jq -r .name \
                | perl -pe 'if (/-[\w\d]+$/) {s/.*-([\d\w]+)$/$1/} else {$_ = ""}')
+
     if [ -n "$GUID" ]; then
         break
     fi
